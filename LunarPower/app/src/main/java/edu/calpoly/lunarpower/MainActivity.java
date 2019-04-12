34639da0fa9c16f70e157e49bcda3e2d21373e41
Jannes <jannes.kretschmer@gmx.de>
2019-04-12 15:40:30 -0700

    basic switch of activities

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/MainActivity.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.calpoly.lunarpower;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View e) {
+                // TODO authentication
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
