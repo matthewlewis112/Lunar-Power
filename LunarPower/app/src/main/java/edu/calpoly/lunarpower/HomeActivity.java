f904cc90213e38da3d5081ca359c9f316418e295
Jannes Kretschmer <jannes.kretschmer@gmx.de>
2019-04-21 00:01:07 +0200

    implemented navigation

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/HomeActivity.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/HomeActivity.java
@@ -1,7 +1,12 @@
 package edu.calpoly.lunarpower;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        openFragment(new HomeFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigationView);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        openFragment(new HomeFragment());
+                        return true;
+                    case R.id.nav_history:
+                        openFragment(new HistoryFragment());
+                        return true;
+                    case R.id.nav_devices:
+                        openFragment(new DeviceFragment());
+                        return true;
+                    case R.id.nav_settings:
+                        openFragment(new SettingsFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
