6ddb6631bd7b4460de2d216e978c32a3f8e4677e
kelsi <kelsi027@gmail.com>
2019-05-06 12:13:10 -0700

    Login Basics push

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/MainActivity.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/MainActivity.java
@@ -5,23 +5,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private TextView attemptsInfo;
+    private Button login;
+    private int counter = 5;        //users are given 5 attempts before lock out
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button login = findViewById(R.id.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        //attempts = (TextView) findViewById(R.id.attempts);
+        login = findViewById(R.id.login);
+
+        attemptsInfo.setText("");   //display nothing until a failed attempt
+
         login.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View e) {
-                // TODO authentication
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                MainActivity.this.startActivity(intent);
+                validate(username.getText().toString(), password.getText().toString());
             }
         });
+    }
+
+    private void validate(String username, String password){
+        // TODO authentication
+        if((username.equals("Admin")) && (password.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            MainActivity.this.startActivity(intent);
+        }
+        else{
+            counter--;
+
+            attemptsInfo.setText("attempts remaining: " + String.valueOf(counter));
+
+            if(counter == 0){
+                login.setEnabled(false);
+            }
+        }
     }
 }
