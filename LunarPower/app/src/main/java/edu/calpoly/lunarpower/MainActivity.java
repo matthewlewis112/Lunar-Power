f6f82c13317609b52314559bb9d1169d21f09639
kretschmer <jannes.kretschmer@heroes.eu>
2019-05-23 00:49:17 +0200

    volley added

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/MainActivity.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/MainActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText username;
@@ -28,15 +35,31 @@ public class MainActivity extends AppCompatActivity {
                 validate(username.getText().toString(), password.getText().toString());
             }
         });
+
     }
 
-    private void validate(String username, String password){
-        // authentication dummy
-        if(username.equals("k")){
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-            MainActivity.this.startActivity(intent);
-        }
-        // avoid codesmell, to be removed:
-        this.password.setText(password);
+    private void validate(final String username,final String password){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // "true".equals(response)
+                        if(username.equals("k")){
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            MainActivity.this.startActivity(intent);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //username.setText("That didn't work!" + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
     }
 }
