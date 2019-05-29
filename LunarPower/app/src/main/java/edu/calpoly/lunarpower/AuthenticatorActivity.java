5f6d3d64a8541f41312f066e0b19c9dfeb9a5e27
Matthew Lewis <matthewlewis112@gmail.com>
2019-05-29 01:22:06 -0700

    Sign Out code in progress

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/AuthenticatorActivity.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/AuthenticatorActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.SignInStateChangeListener;
 import com.amazonaws.mobile.auth.ui.SignInUI;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
@@ -17,12 +20,22 @@ public class AuthenticatorActivity extends Activity {
         setContentView(R.layout.activity_authenticator);
 
         // Add a call to initialize AWSMobileClient
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+        AWSMobileClient.getInstance().initialize(this).execute();
+        IdentityManager.getDefaultIdentityManager().addSignInStateChangeListener(new SignInStateChangeListener() {
             @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
+            public void onUserSignedOut() {
+                Log.d("AuthenticatorActivity", "User Signed Out");
                 SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
                 signin.login(AuthenticatorActivity.this, HomeActivity.class).execute();
             }
-        }).execute();
+
+            @Override
+            public void onUserSignedIn() {
+                Log.d("AuthenticatorActivity", "User Signed In");
+            }
+        });
+        Log.d("AuthenticatorActivity", "User Signed Out");
+        SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+        signin.login(AuthenticatorActivity.this, HomeActivity.class).execute();
     }
 }
--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/SettingsFragment.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/SettingsFragment.java
@@ -1,6 +1,9 @@
 package edu.calpoly.lunarpower;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,10 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amazonaws.mobile.auth.core.IdentityManager;
+
 public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        Button logoutBtn = (Button) rootView.findViewById(R.id.logout);
+        logoutBtn.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.d("Settings","Logging Out");
+                IdentityManager.getDefaultIdentityManager().signOut();
+            }
+        });
         return inflater.inflate(R.layout.fragment_settings,container,false);
     }
 }
