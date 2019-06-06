7c9b9646dc906ceaae24be5c40885b29749dfce5
Matthew Lewis <matthewlewis112@gmail.com>
2019-06-06 00:42:48 -0700

    Resolving #76

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/AddDeviceFragment.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/AddDeviceFragment.java
@@ -8,14 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
 
 public class AddDeviceFragment extends Fragment {
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Nullable
     @Override
