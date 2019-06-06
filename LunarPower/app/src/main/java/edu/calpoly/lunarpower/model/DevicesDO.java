6d1a0cab34f41eb3f464d0c0c7126250063db92b
Matthew Lewis <fallout@localhost.localdomain>
2019-06-06 11:48:15 -0700

    Working on #78

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/model/DevicesDO.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/model/DevicesDO.java
@@ -2,14 +2,8 @@ package edu.calpoly.lunarpower.model;
 
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @DynamoDBTable(tableName = "lunarpower-mobilehub-627565767-Devices")
 
