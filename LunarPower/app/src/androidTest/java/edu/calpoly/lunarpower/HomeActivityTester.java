3f90a5225e9788b4d479ef39d3423ac5d1d89c63
Matthew Lewis <fallout@localhost.localdomain>
2019-06-06 21:57:09 -0700

    Test case v2

--- LunarPower/app/src/androidTest/java/edu/calpoly/lunarpower/HomeActivityTester.java
+++ LunarPower/app/src/androidTest/java/edu/calpoly/lunarpower/HomeActivityTester.java
@@ -2,6 +2,7 @@ package edu.calpoly.lunarpower;
 
 import android.content.Context;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.Fragment;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,9 +10,25 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class HomeActivityTester {
+    private static final String packageName = "edu.calpoly.lunarpower";
+    @Test
+    public void testHomeActivity(){
+        Context appContext = new HomeActivity();
+        Assert.assertEquals(packageName, appContext.getPackageName());
+    }
     @Test
     public void testHomeFragment(){
-        Context appContext = new HomeActivity();
-        Assert.assertEquals("edu.calpoly.lunarpower", appContext.getPackageName());
+        Fragment fragment= new HomeFragment();
+        Assert.assertEquals(packageName, fragment.getActivity().getPackageName());
+    }
+    @Test
+    public void testDeviceFragment(){
+        Fragment fragment= new DeviceFragment();
+        Assert.assertEquals(packageName, fragment.getActivity().getPackageName());
+    }
+    @Test
+    public void testSettingsFragment(){
+        Fragment fragment = new SettingsFragment();
+        Assert.assertEquals(packageName, fragment.getActivity().getPackageName());
     }
 }
