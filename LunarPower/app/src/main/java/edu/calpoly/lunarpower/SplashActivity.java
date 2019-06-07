3584ec1b10a8b4c9d8272971c1b35c7fb1b007eb
Matthew Lewis <matthewlewis112@gmail.com>
2019-06-06 18:51:52 -0700

    Code smells related to Splash Activity

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/SplashActivity.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/SplashActivity.java
@@ -3,13 +3,13 @@ package edu.calpoly.lunarpower;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import com.amazonaws.mobile.auth.core.IdentityManager;
-import com.amazonaws.mobile.auth.core.StartupAuthResult;
-import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
 import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSStartupHandler;
-import com.amazonaws.mobile.client.AWSStartupResult;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
+
+import static com.amazonaws.mobile.client.UserState.SIGNED_IN;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,24 +18,26 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        AWSMobileClient.getInstance().initialize(SplashActivity.this, new AWSStartupHandler() {
+        AWSMobileClient.getInstance().initialize(SplashActivity.this, new Callback<UserStateDetails>() {
+            final private String logTag = "Splash Activity";
             @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
-                IdentityManager identityManager = IdentityManager.getDefaultIdentityManager();
-                identityManager.resumeSession(SplashActivity.this, new StartupAuthResultHandler() {
-                    @Override
-                    public void onComplete(StartupAuthResult authResults) {
-                        if (authResults.isUserSignedIn()) {
-                            Intent intent = new Intent(SplashActivity.this, HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            intent.putExtra("frag","HomeFragment");
-                            startActivity(intent);
-                        } else {
-                            startActivity(new Intent(SplashActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                        }
-                    }
-                }, 3000);
+            public void onResult(UserStateDetails result) {
+                if (result.getUserState() == SIGNED_IN) {
+                    Log.d(logTag, "Signed in");
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.putExtra("frag","HomeFragment");
+                    startActivity(intent);
+                } else {
+                    Log.d(logTag, "Need to sign in");
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                }
             }
-        }).execute();
+
+            @Override
+            public void onError(Exception e) {
+                Log.d(logTag, "Error signing in: " + e.getMessage());
+            }
+        });
 
     }
 }
