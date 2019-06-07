5464e0cc6fcab9a302d3544ea0ad57f2f6d46ff4
kretschmer <jannes.kretschmer@heroes.eu>
2019-06-06 21:35:47 -0700

    added task list fragment

--- LunarPower/app/src/main/java/edu/calpoly/lunarpower/HomeFragment.java
+++ LunarPower/app/src/main/java/edu/calpoly/lunarpower/HomeFragment.java
@@ -7,13 +7,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View inflate = inflater.inflate(R.layout.fragment_home, container, false);
+        Button getTasks = inflate.findViewById(R.id.get_tasks);
+        final Date date = new Date();
+        final CalendarView calendar = inflate.findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Calendar tmp = Calendar.getInstance();
+                tmp.set(Calendar.YEAR, year);
+                tmp.set(Calendar.MONTH, month);
+                tmp.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                date.setTime(tmp.getTimeInMillis());
+            }
+        });
+        getTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((HomeActivity)getActivity()).openFragment(new TaskListFragment(date));
+            }
+        });
+        return inflate;
     }
 }
