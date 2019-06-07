c64f079426905bad5ac7f8ddebd1e747036abef0
Matthew Lewis <fallout@localhost.localdomain>
2019-06-07 01:52:57 -0700

    Test 1

---
+++ LunarPower/app/src/test/java/edu/calpoly/lunarpower/DeviceFragmentTest.java
@@ -0,0 +1,38 @@
+package edu.calpoly.lunarpower;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.widget.LinearLayout;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import edu.calpoly.lunarpower.model.DevicesDO;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeviceFragmentTest {
+    private static final String packageName = "edu.calpoly.lunarpower";
+    @Test
+    public void testPackage() {
+        Fragment appFragment = new DeviceFragment();
+        assertEquals(packageName, appFragment.getActivity().getPackageName());
+
+    }
+    @Test
+    public void testLayout() {
+        assertEquals(new DeviceFragment().getLayoutInflater().getClass(), LinearLayout.class);
+    }
+    @Test
+    public void testParentClass() {
+        assertEquals(new DeviceFragment().getClass(), DeviceFragment.class);
+    }
+    @Test
+    public void testFragment() {
+        assertEquals(new HomeActivity().getDevices().size(), 0);
+    }
+
+
+
+}
